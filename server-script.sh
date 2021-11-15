c5585b1a4ef5557850676be293392aa8495b351c
EklavyaNagpal <35739589+EklavyaNagpal@users.noreply.github.com>
2021-11-16 05:21:16 +0530

    Delete server-script.sh

--- server-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum update                     # Update the system
-sudo yum install -y httpd           # Install Apache
-sudo systemctl start httpd          # Start Apache
-sudo systemctl enable httpd         # Enable The Server
-
-echo "<h1>Hello from Terraform</h1>" | sudo tee /var/www/html/index.html
